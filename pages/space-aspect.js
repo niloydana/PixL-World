35c7e493dac608c6dfc8ccb6829a1eac8f0a6fd8
Soham <chitradana81@gmail.com>
2022-03-16 11:08:05 +0530

    added more js

--- pages/space-aspect.js
+++ pages/space-aspect.js
@@ -1,6 +1,18 @@
 const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-console.log(canvas);
+class Player {
+  constructor(x, y, radius, colour) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.colour = colour;
+  }
+}
+
+const player = new Player(100, 100, 30, "blue");
+
+console.log(player);
