b98de09692b2828d5479d1a53dcdeb1f3f8c44d9
Soham <chitradana81@gmail.com>
2022-03-16 11:29:50 +0530

    updated game

--- pages/space-aspect.js
+++ pages/space-aspect.js
@@ -20,10 +20,27 @@ class Player {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, colour, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.colour = colour;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.colour;
+    ctx.fill();
+  }
+}
+
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, "#D7DAE5");
+const player = new Player(x, y, 30, "#16BAC5");
 player.draw();
 
 console.log(player);
